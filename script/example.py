5c43f5cdcab7f1fedd284b62831eb33480fa5cbe
Roetta Marco <marcoroe11@gmail.com>
2014-12-01 18:37:59 +0100

    Creata directory script Phyton
    Questa nuova directory conterrà gli script da usare internamente come
    ausilio alal gestione della documentazione.

---
+++ script/example.py
@@ -0,0 +1,15 @@
+# ---
+# Script di prova che fa operazioni basilari.
+# Verranno aggiunte qui tutte le funzioni ritenute importanti.
+# ---
+import sys
+print "Something's rotten in the state of Denmark."
+print "                -- Shakespeare"
+print
+print "La dimensione massima di un intero: ", sys.maxint
+print "La dimensione massima di una word: ", sys.maxsize
+print "sys.exit(0) significa successo, una valore positivo diverso significa errore"
+print
+sys.exit(0)
+
+
