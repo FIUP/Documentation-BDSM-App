f7f4737d0cb81c7d7a852ae90644c7ceeb1566bd
Tesser Paolo <p.tesser921@gmail.com>
2014-12-22 12:55:23 +0100

    Title: test post-commit hooks Desc: hooks per automatizzare la gestione dei ticket
    Task: Reported in #22848685719318
    Option: [complete]
    END

--- script/asana_api/option_commit_msg.py
+++ script/asana_api/option_commit_msg.py
@@ -7,18 +7,21 @@ import sys
 
 
 # MAIN
+my_api_asana = my_api.MyAPI()
 
 print "Execution script from branch: ", sys.argv[1]
-print "List of commit message: "
-print sys.argv[2]
+# print "List of commit message: "
+# print sys.argv[2]
 
 list_commit_queue = sys.argv[2].split("END")
-print list_commit_queue
+# print list_commit_queue
 
 for commit_msg in list_commit_queue:
     if commit_msg != "":  # per non eseguire le istruzioni sull'ultimo valore che è sempre vuoto
-        print 1
-
+        list_message = commit_msg.splitlines()
+        for line in list_message:
+            line.trim()
+            print line
 
 checkout_asana_cmd = ["git", "checkout", "script/asana_api"]
 subprocess.call(checkout_asana_cmd)
