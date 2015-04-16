f163092867707a7b5a76b43850172d7296b32d59
Tesser Paolo <p.tesser921@gmail.com>
2015-04-16 19:05:28 +0200

    Title: [doc] migliorata desc tecnologie utilizzate Desc: Task: Reported in #id_task Option: [option][option] END

---
+++ script/tracciamento/componenti.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Name: nome_file.py
+# Author: Tesser Paolo
+# Date: aaaa-mm-dd
+# Mail: p.tesser921@gmail.com
+# Modify
+# Version   Date        Author          Description
+# =======================================================================
+# 
+# -----------------------------------------------------------------------
+#
+# -----------------------------------------------------------------------
+#
+#
+__author__ = 'ptesser'
