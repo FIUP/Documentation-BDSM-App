6d40e7a8f2dc6e1e5aef7ed493de2d2e79543efc
Tesser Paolo <p.tesser921@gmail.com>
2015-05-22 15:06:27 +0200

    Title: [script] iniziata stesura track classi-req Desc: effettuato anche un po' di refactor dei vecchi script Task: Reported in #id_task Option: [option][option] END

--- script/tracciamento/requisiti_classi.py
+++ script/tracciamento/requisiti_classi.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Name: tracciamento_st.py
+# Name: nome_file.py
 # Author: Tesser Paolo
 # Date: aaaa-mm-dd
-# Desc: this script starts when make command it's start to generate Spefica Tecnica document.
-# It takes value from componenti-requisiti.tex file and generate viceversa table.
-#
 # Mail: p.tesser921@gmail.com
 # Modify
 # Version   Date        Author          Description
 # =======================================================================
-# 
+#
 # -----------------------------------------------------------------------
 #
 # -----------------------------------------------------------------------
@@ -21,16 +18,13 @@ __author__ = 'ptesser'
 from requisiti_fonti import RequisitiFonti
 
 if __name__ == '__main__':
-
-    print "*** Start: generazione file contenente lista requisiti tracciati con i componenti " \
-          "presi da componenti-requisiti.text"
+    print "*** Start: generazione file contenente lista requisiti per tracciamento requisiti-classi"
 
     r = RequisitiFonti()
     r.open_doc_req()
-    r.open_doc_componenti()
+    r.open_doc_classi()
     r.take_req()
     r.take_req_comp()
-    r.write_list_req_for_comp()
+    r.write_list_req_for_class()
 
-    print "*** End: generazione file contenente lista requisiti tracciati con i componenti " \
-          "presi da componenti-requisiti.text"
+    print "*** End: generazione file contenente lista requisiti per tracciamento requisiti-classi"
